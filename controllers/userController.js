34beaf052344e020a2912208db8b4ebac4b4780a
rosaalwyh <alawiyahrosa@gmail.com>
2022-02-23 21:31:15 +0700

    skeleton-user

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,5 +1,27 @@
 class UserController {
+    static getUser(req, res) {
 
+    }
+
+    static addUser(req, res) {
+        
+    }
+
+    static createUser(req, res) {
+        
+    }
+
+    static editUser(req, res) {
+        
+    }
+
+    static updateUser(req, res) {
+        
+    }
+
+    static deleteUser(req, res) {
+        
+    }
 }
 
 module.exports = UserController
