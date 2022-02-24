fd040c1be82c49076a7d51dc3a5e9bd6fa154b43
rosaalwyh <alawiyahrosa@gmail.com>
2022-02-25 00:41:46 +0700

    view user

--- routes/detailUserRoutes.js
+++ routes/detailUserRoutes.js
@@ -2,11 +2,11 @@ const detailUserRouter = require('express').Router();
 const DetailUserController =  require('../controllers/detailUserController');
 const { isAdmin } = require('../middleware');
 
-detailUserRouter.get('/detailUser', isAdmin, DetailUserController.getdetailUser)
 detailUserRouter.get('/detailUser/add', isAdmin, DetailUserController.addDetailUser)
 detailUserRouter.post('/detailUser/add', isAdmin, DetailUserController.createDetailUser)
 detailUserRouter.get('/detailUser/edit/:id', isAdmin, DetailUserController.editDetailUser)
 detailUserRouter.post('/detailUser/edit/:id', isAdmin, DetailUserController.updateDetailUser)
 detailUserRouter.get('/detailUser/delete/:id', isAdmin, DetailUserController.deleteDetailUser)
+detailUserRouter.get('/detailUser/:id', isAdmin, DetailUserController.getdetailUser)
 
 module.exports = detailUserRouter
